204d4f648d7a2b26ad0d838859a35dc66c0e996d
869621380 <149985202+869621380@users.noreply.github.com>
2024-01-30 02:52:01 +0800

    1
    1

---
+++ Project1/Project1/FileName.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    srand(time(0));
+    int a,b,c;
+    a = rand()%7+1;
+    b = rand() % 100 + 1;
+    cout << a <<" "<<b;
+
+
+    return 0;
+}
